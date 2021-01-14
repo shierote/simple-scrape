d9fd5f46dff184ebedfb709bb66e82bdefede3f5
shierote <taishi2060@gmail.com>
2021-01-14 23:37:49 +0900

    :tada: First commit

---
+++ simple-scrape/__init__.py
@@ -0,0 +1,10 @@
+from .<ファイル> import *
+
+__copyright__    = 'Copyright (C) 2021 Taishi Eguchi'
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'Taishi Eguchi'
+__author_email__ = 'taishi2060@gmail.com'
+__url__          = 'http://github.com/account/repository'
+
+__all__ = ['公開モジュール', ...]
